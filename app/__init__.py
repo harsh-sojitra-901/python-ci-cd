dbfe5eb7a33e94e6590759b2310d9ae1acde5e28
harsh-sojitra-901 <harshsojitra490@gmail.com>
2026-05-22 18:38:45 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask, jsonify
 
-
 def create_app():
     app = Flask(__name__)
 
     @app.get("/")
     def home():
-        return jsonify(
-            {
-                "message": "Hello from Flask CI/CD demo!",
-                "status": "ok",
-            }
-        )
+        return jsonify({
+            "message": "Hello from Flask CI/CD demo!",
+            "status": "ok",
+        })
 
     @app.get("/health")
     def health_check():
@@ -19,6 +16,9 @@ def create_app():
 
     @app.get("/api/version")
     def version():
-        return jsonify({"app": "flask-ci-cd-demo", "version": "1.0.0"})
+        return jsonify({
+            "app": "flask-ci-cd-demo",
+            "version": "1.0.0"
+        })
 
     return app
